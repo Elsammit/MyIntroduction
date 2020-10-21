e2457dcca93d3ea5b35d5e3a7fc6b549d4b9ed86
Elsammit <hhashimo25@gmail.com>
2020-10-21 20:28:43 +0900

    add BrowserRouter

--- src/library.js
+++ src/library.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Top from './top';
 import Main from './mainpage'
+import "./mainpage.css";
 import Profile from './profile'
 export default class waniwani extends Component  {
     constructor (props) {
@@ -14,7 +15,7 @@ export default class waniwani extends Component  {
 
     render() {
         return (<div>
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div>
                     <Route exact path='/' component={Main} />
                     <Route path='/MakeGame' component={Top} />
