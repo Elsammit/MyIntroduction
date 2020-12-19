ab9c7c6dc58d758625b904fbac8450d82e81fc89
Elsammit <hhashimo25@gmail.com>
2020-12-19 10:02:56 +0900

    ソースコード整理

--- src/top.js
+++ src/top.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import posed from 'react-pose';
 import "./top.css";
 import WaniImg from "./image/waniImg.gif"
 import MoguraImg from "./image/moguratataki.gif"
@@ -8,13 +9,33 @@ import HatenaGame from "./image/hatena.gif"
 import Cooking from "./image/cooking.gif"
 import WordApp from "./image/wordApp.gif"
 import Profile from "./image/profile.png"
+const sidebarProps = {
+    open: {
+      x: '10px',
+      delayChildren: 300,
+      staggerChildren: 100,
+    },
+    closed: {
+      delay: 500,
+      staggerChildren: 20,
+      x: '-100%'
+    }
+  };
+  
+  const itemProps = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: 10 }
+  };
+const SidePanel = posed.ul(sidebarProps);
+const Item = posed.li(itemProps);
 export default class TopPage extends Component  {
     constructor (props) {
         super(props);
         this.state = {
             location:0,
+            selectcombo:0,
+            isOpen: false,
         };
-        //this.test = this.onChange.bind(this);
     }
 
     Header = () =>{
@@ -70,54 +91,109 @@ export default class TopPage extends Component  {
         )
     }
 
-    test = (e) =>{
-        console.log(e)
-        var sample = document.getElementById("tt");
-        console.log('text = ' + sample.value);
-    }
+    clicked = (val) =>{
+        console.log(val)
+        var buf = 0
+        switch(val){
+            case "WebApp":
+                buf = 0;
+                break;
+            case "ImageApp":
+                buf = 1;
+                break;
+            default:
+                buf = -1;
+        }
+        this.setState({
+            selectcombo:buf
+        })
+    } 
 
     ReactAppPage = () =>{
         return(
-        <div className="article" >
-            {this.MakeAppView("わにたたき", "某ゲームのワニたたき！！ワニを叩くと音声も出るよｗｗ", WaniImg, 
+            <div className="article" >
+                {this.MakeAppView("わにたたき", "某ゲームのワニたたき！！ワニを叩くと音声も出るよｗｗ", WaniImg, 
                         "ワニたたき画像", "https://elsammit.github.io/waniwanipanic/", "AppImg")}
-            {this.MakeAppView("モグラたたき", "モグラたたきゲーム<br/>結構上手く作れた気がする", MoguraImg, 
+                {this.MakeAppView("モグラたたき", "モグラたたきゲーム<br/>結構上手く作れた気がする", MoguraImg, 
                         "モグラたたき画像", "https://elsammit.github.io/Moguratataki/", "AppImg")}
-            {this.MakeAppView("花火", "花火も作ってみた<br/>今年はいけなかったからせめてWeb上だけでも", FireWork, 
+                {this.MakeAppView("花火", "花火も作ってみた<br/>今年はいけなかったからせめてWeb上だけでも", FireWork, 
                         "花火画像", "https://elsammit.github.io/Fireworks/", "AppImg2")}
-            {this.MakeAppView("ポケモンステータス確認フォーム", "ポケモンステータス確認用フォームです。\n 第1世代～第7世代のステータスや努力値振りをレーダーチャートで分かりやすくしてみました", Pokemon, 
+                {this.MakeAppView("ポケモンステータス確認フォーム", "ポケモンステータス確認用フォームです。\n 第1世代～第7世代のステータスや努力値振りをレーダーチャートで分かりやすくしてみました", Pokemon, 
                         "ポケモン画像", "https://elsammit.github.io/PokeChart/", "AppImg2")}                               
-            {this.MakeAppView("お料理ルーレットアプリ", "本日の献立を選んでくれるアプリです。\nちょっと献立数が少ないかな？？", Cooking, 
+                {this.MakeAppView("お料理ルーレットアプリ", "本日の献立を選んでくれるアプリです。\nちょっと献立数が少ないかな？？", Cooking, 
                         "お料理ゲーム画像", "https://elsammit.github.io/CookingApp/", "AppImg3")}     
-            {this.MakeAppView("誰が出てきたか当てるゲーム", "誰が出てきたか当てるゲームです\nワニたたき作成している時に作ってみたゲームだけど。。。</th>", HatenaGame, 
+                {this.MakeAppView("誰が出てきたか当てるゲーム", "誰が出てきたか当てるゲームです\nワニたたき作成している時に作ってみたゲームだけど。。。</th>", HatenaGame, 
                         "よくわからないゲーム画像", "https://elsammit.github.io/hitSomeone/", "AppImg3")}  
-            {this.MakeAppView("英単語アプリ", "単語を勉強するためのアプリ\n単語の追加変更欄も追加しているので集中的な勉強も出来ます！！", WordApp, 
+                {this.MakeAppView("英単語アプリ", "単語を勉強するためのアプリ\n単語の追加変更欄も追加しているので集中的な勉強も出来ます！！", WordApp, 
+                        "英単語画像", "https://elsammit.github.io/wordbookApp/", "AppImg2")}  
+            </div>
+        )
+    }
+
+    ImageAppPage = () =>{
+        return(
+            <div className="article" >
+                {this.MakeAppView("わにたたき", "某ゲームのワニたたき！！ワニを叩くと音声も出るよｗｗ", WaniImg, 
+                        "ワニたたき画像", "https://elsammit.github.io/waniwanipanic/", "AppImg")}
+                {this.MakeAppView("モグラたたき", "モグラたたきゲーム<br/>結構上手く作れた気がする", MoguraImg, 
+                        "モグラたたき画像", "https://elsammit.github.io/Moguratataki/", "AppImg")}
+                {this.MakeAppView("花火", "花火も作ってみた<br/>今年はいけなかったからせめてWeb上だけでも", FireWork, 
+                        "花火画像", "https://elsammit.github.io/Fireworks/", "AppImg2")}
+                {this.MakeAppView("ポケモンステータス確認フォーム", "ポケモンステータス確認用フォームです。\n 第1世代～第7世代のステータスや努力値振りをレーダーチャートで分かりやすくしてみました", Pokemon, 
+                        "ポケモン画像", "https://elsammit.github.io/PokeChart/", "AppImg2")}                               
+                {this.MakeAppView("お料理ルーレットアプリ", "本日の献立を選んでくれるアプリです。\nちょっと献立数が少ないかな？？", Cooking, 
+                        "お料理ゲーム画像", "https://elsammit.github.io/CookingApp/", "AppImg3")}     
+                {this.MakeAppView("誰が出てきたか当てるゲーム", "誰が出てきたか当てるゲームです\nワニたたき作成している時に作ってみたゲームだけど。。。</th>", HatenaGame, 
+                        "よくわからないゲーム画像", "https://elsammit.github.io/hitSomeone/", "AppImg3")}  
+                {this.MakeAppView("英単語アプリ", "単語を勉強するためのアプリ\n単語の追加変更欄も追加しているので集中的な勉強も出来ます！！", WordApp, 
                         "英単語画像", "https://elsammit.github.io/wordbookApp/", "AppImg2")}  
         </div>
         )
     }
 
+    toggle = () => this.setState({ isOpen: !this.state.isOpen });
+    SelectApp = () =>{
+        return(      
+            <div width="250px">
+                <button onClick={this.toggle} className="asideSelectButton">Click Here !!</button>
+                <SidePanel
+                    onClick={this.toggle}
+                    className="sidebar" 
+                    pose={this.state.isOpen ? 'open' : 'closed'}
+                >
+                    <Item className="item" value="WebApp" onClick={() => this.clicked("WebApp")}>Webアプリ</Item>
+                    <Item className="item" value="ImageApp" onClick={() => this.clicked("ImageApp")}>画像処理</Item>
+                    <Item className="item" value="Python" onClick={() => this.clicked("Python")}>python</Item>
+                    <Item className="item" value="Golang" onClick={() => this.clicked("Golang")}>go言語</Item>
+                    <Item className="item" value="MachineLearn" onClick={() => this.clicked("MachineLearn")}>機械学習</Item>
+                </SidePanel>
+            </div>)
+    }
+
     render() {
+        const {selectcombo} = this.state;
+        console.log(selectcombo)
         return (<div>
             {this.Header()}
             <div className="Introduce">
-                <div className="leftaside" >
-                <select name="blood" id="tt" onChange={this.test}>
-                    <option value="Aries">おひつじ座</option>  
-                    <option value="Taurus">おうし座</option>
-                    <option value="Gemini">ふたご座</option>
-                    <option value="Cancer">かに座</option>
-                    <option value="Leo">しし座</option>
-                    <option value="Virgo">おとめ座</option>
-                    <option value="Libra">てんびん座</option>
-                    <option value="Scorpio">さそり座</option>
-                    <option value="Saggitarius">いて座</option>
-                    <option value="Capricorn">やぎ座</option>
-                    <option value="Aquarius">みずがめ座</option>
-                    <option value="Pisces">うお座</option>
-                    </select>
-                </div>
-                {this.ReactAppPage()}
+                {this.SelectApp()}
+                <>
+                {
+                    (() =>{
+                        if(selectcombo == 0){
+                            return(<div>
+                                    {this.ReactAppPage()}
+                                </div>)
+                        }else if(selectcombo == 1){
+                            return(<div>
+                                    {this.ImageAppPage()}
+                                </div>
+                            )
+                        }
+                    })()
+                }
+                </>
+                
                 <div className="aside" >
                     {this.AsidePage()}
                 </div>
